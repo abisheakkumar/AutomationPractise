b62a5031756d43f678155c114c82ed8a5ba1263f
abisheakkumar <abisheakkumar29@gmail.com>
2024-01-18 09:19:19 +0530

    Commit and push 3

--- src/test/java/practise/LaunchingPrinting.java
+++ src/test/java/practise/LaunchingPrinting.java
@@ -3,5 +3,6 @@ package practise;
 public class LaunchingPrinting {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Hello");
 	}
 }
--- src/test/java/practise/LaunchingPrinting1.java
+++ src/test/java/practise/LaunchingPrinting1.java
@@ -3,5 +3,6 @@ package practise;
 public class LaunchingPrinting1 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hi");
 	}
 }
